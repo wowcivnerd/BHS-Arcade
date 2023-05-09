5d191484f61f8ddc01700d1745242449435734c7
Ollie Brown <6789oliver@gmail.com>
2023-05-10 11:27:07 +1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    # games_folder = os.path.join('static', 'games')
+    # game_files = os.listdir(games_folder)
+    # game_name = 
+    # game_location = 
+
+        games_folder = os.path.join('static', 'games')
+        game_files = [os.path.join(games_folder, file) for file in os.listdir(games_folder)]
+        return render_template('index.html', game_files=game_files)
+
+
+    
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8008,debug=True)
